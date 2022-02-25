4c16adedab96330ce8c5e10b25fa857c94af16f6
Lakshmi Durga <85387346+Lakshmi-200@users.noreply.github.com>
2022-02-25 18:33:06 +0530

    import java.util.*; class Division {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int m=s.nextInt();         int div=n/m;         System.out.println(div);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Division
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+        int div=n/m;
+        System.out.println(div);
+    }
+}
